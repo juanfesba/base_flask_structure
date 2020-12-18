be692b57c4d10a321ef2482fe5cb2cc0708717cd
Juan F. Escobar <juanfesba@hotmail.com>
2020-12-18 12:19:04 -0500

    simple test

--- src/tests/test_webapp.py
+++ src/tests/test_webapp.py
@@ -1,9 +1,11 @@
 from src import routing
+from flask import render_template
 import unittest
 
 class Test_TestIncrementDecrement(unittest.TestCase):
     def test_increment(self):
-        self.assertEqual(routing.home(), 4)
+        with routing.app.test_request_context(): #with app.app_context(), app.test_request_context():
+            self.assertEqual(routing.home(), render_template("home_page.html", name=None))
 
 if __name__ == '__main__':
     unittest.main()
