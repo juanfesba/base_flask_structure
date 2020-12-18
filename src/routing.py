5732de5eddb8c8ac835e5b0846b7b01544957667
Juan F. Escobar <juanfesba@hotmail.com>
2020-12-18 00:50:14 -0500

    more prep

--- src/routing.py
+++ src/routing.py
@@ -4,3 +4,11 @@ from . import app
 @app.route("/")
 def home():
     return render_template("home_page.html", name=None)
+
+@app.route("/hello/")
+@app.route("/hello/<name>")
+def hello_there(name = None):
+    return render_template(
+            "hello_there.html",
+            name=name
+    )
