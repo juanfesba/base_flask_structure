85d302bac9d6b2e315b086914d65e6bb5e7f8464
Juan F. Escobar <juanfesba@hotmail.com>
2020-12-19 21:53:44 -0500

    blueprints

--- src/routing.py
+++ src/routing.py
@@ -1,17 +1,8 @@
 from . import app
-from flask import render_template
-#import logging
+from flask import render_template, Blueprint
 
+bp = Blueprint('home_page', __name__)
 
-@app.route("/")
+@bp.route("/")
 def home():
-    #logging.warning("hey")
     return render_template("home_page.html", name=None)
-
-@app.route("/hello/")
-@app.route("/hello/<name>")
-def hello_there(name = None):
-    return render_template(
-            "hello_there.html",
-            name=name
-    )
