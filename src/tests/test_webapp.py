16a673aa39bf30b02068bc0a18797206eed65044
Juan F. Escobar <juanfesba@hotmail.com>
2020-12-21 13:09:30 -0500

    fix tests

--- src/tests/test_webapp.py
+++ src/tests/test_webapp.py
@@ -1,10 +1,12 @@
-from src import routing
-from flask import render_template
 import unittest
+
+from flask import render_template
+from src import app, routing
+
 
 class Test_TestIncrementDecrement(unittest.TestCase):
     def test_increment(self):
-        with routing.app.test_request_context(): #with app.app_context(), app.test_request_context():
+        with app.test_request_context(): #with app.app_context(), app.test_request_context():
             self.assertEqual(routing.home(), render_template("home_page.html", name=None))
 
 if __name__ == '__main__':
