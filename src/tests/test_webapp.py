e9e00033c144cbefc2c6caad6ce23294a5d8dcf1
Juan F. Escobar <juanfesba@hotmail.com>
2020-12-21 13:10:57 -0500

    renaming

--- src/tests/test_webapp.py
+++ src/tests/test_webapp.py
@@ -1,13 +1,12 @@
 import unittest
 
 from flask import render_template
-from src import app, routing
-
+from src import app, home_page
 
 class Test_TestIncrementDecrement(unittest.TestCase):
     def test_increment(self):
         with app.test_request_context(): #with app.app_context(), app.test_request_context():
-            self.assertEqual(routing.home(), render_template("home_page.html", name=None))
+            self.assertEqual(home_page.home(), render_template("home_page.html", name=None))
 
 if __name__ == '__main__':
     unittest.main()
