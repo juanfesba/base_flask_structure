479670c073bea1895b82a148e96924188dc1627d
Juan F. Escobar <juanfesba@hotmail.com>
2020-12-23 23:32:58 -0500

    secret key from env

--- src/webchat.py
+++ src/webchat.py
@@ -1,5 +1,10 @@
 # Entry point for the application.
+import os
+
 from . import app    # For application discovery by the 'flask' command.
+app.config.from_mapping(
+    SECRET_KEY=os.environ.get('SECRET_KEY')
+)
 
 from . import home_page  # For import side-effects of setting up routes.
 app.register_blueprint(home_page.bp)
